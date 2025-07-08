781d4a3b7a28a36be1edf178464d68e391f320f1
psvm203 <psvm203@proton.me>
2025-07-08 22:25:28 +0900

    feat: add theme controller

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,81 @@
+use gloo_storage::{LocalStorage, Storage};
+use serde::Deserialize;
 use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
+    let theme_controller = {
+        const THEME_STORAGE_KEY: &str = "theme";
+        const THEME_DEFAULT_VALUE: &str = "default";
+
+        #[derive(Clone, Deserialize)]
+        struct Theme {
+            value: String,
+            name: String,
+        }
+
+        let initial_theme = LocalStorage::get::<String>(THEME_STORAGE_KEY)
+            .unwrap_or_else(|_| THEME_DEFAULT_VALUE.to_owned());
+
+        let theme_state = use_mut_ref(|| initial_theme);
+
+        let theme_item = |theme: &Theme| -> Html {
+            let on_theme_change = {
+                let theme_state = theme_state.clone();
+                let theme_value = theme.value.clone();
+
+                move |_| {
+                    *theme_state.borrow_mut() = theme_value.clone();
+                    LocalStorage::set(THEME_STORAGE_KEY, theme_value.clone()).unwrap();
+                }
+            };
+
+            html! {
+                <li key={theme.value.clone()}>
+                    <input
+                        type={"radio"}
+                        name={"theme-dropdown"}
+                        class={"theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"}
+                        aria-label={theme.name.clone()}
+                        value={theme.value.clone()}
+                        checked={theme.value == *theme_state.borrow()}
+                        onchange={on_theme_change}
+                    />
+                </li>
+            }
+        };
+
+        let theme_data = include_str!("theme.yaml");
+        let themes: Vec<Theme> = serde_yaml::from_str(theme_data).unwrap();
+        let theme_items: Html = themes.into_iter().map(|theme| theme_item(&theme)).collect();
+
+        html! {
+            <div class={"dropdown mb-72"}>
+                <div tabindex={"0"} role={"button"} class={"btn m-1"}>
+                    { "테마" }
+                    <svg
+                        width={"12px"}
+                        height={"12px"}
+                        class={"inline-block h-2 w-2 fill-current opacity-60"}
+                        xmlns={"http://www.w3.org/2000/svg"}
+                        viewBox={"0 0 2048 2048"}
+                    >
+                        <path d={"M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"} />
+                    </svg>
+                </div>
+                <ul
+                    tabindex={"0"}
+                    class={"dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl"}
+                >
+                    { theme_items }
+                </ul>
+            </div>
+        }
+    };
+
     html! {
         <div>
-            { "Hello, World!" }
+            { theme_controller }
         </div>
     }
 }
